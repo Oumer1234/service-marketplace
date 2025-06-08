6114c557114c3f2e1ef47b1f1468ec8500ef217e
Yeabsira <yabahane@gmail.com>
2025-06-08 12:59:40 +0300

    fix nav

--- service-marketplace/src/app/profile/page.tsx
+++ service-marketplace/src/app/profile/page.tsx
@@ -244,7 +244,7 @@ export default function ProfilePage() {
                     You have a service provider account
                   </p>
                 </div>
-                <Link href={`/service-provider/${user.serviceProviderId}`}>
+                <Link href={`/service-provider/${user.id}`}>
                   <Button size="sm" variant="outline" className="gap-1">
                     Manage services
                     <ArrowRight className="h-4 w-4" />
